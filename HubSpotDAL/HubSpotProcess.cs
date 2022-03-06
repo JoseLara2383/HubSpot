d12f24599da5a2bafb712650385ec0c12e2f6dd5
Edgar Daniel Vazquez Escobedo <edgarsoporteti@gmail.com>
2022-03-05 21:11:57 -0600

    EDVAZQUEZE
    Se suben cmbios sobre conversiones de unix time

--- HubSpotDAL/HubSpotProcess.cs
+++ HubSpotDAL/HubSpotProcess.cs
@@ -11,9 +11,10 @@ namespace HubSpotDAL
 {
     public class HubSpotProcess
     {
-
+       
         public static async Task<string> GetContact()
         {
+            Tools _tools = new Tools();
             SettingSync.getSetting();
             string after = "0";
             //Recorrer los contacts para procesarlos
@@ -31,15 +32,20 @@ namespace HubSpotDAL
                 }
                 else
                     after = string.Empty;
+                
+                //TEST UNIX
+                //var unixTime =_tools .ConvertDateUnixTime(DateTime.Now);
+                //var datetim = _tools.ConvertUnixTimeToDatetime(unixTime);
+
 
 
                 if (contactResult != null)
                     ContactDAL.InsUpdData(SettingSync.SettingHubSpot.ConexionString, contactResult);
-            } while (after!=string.Empty);
+            } while (after != string.Empty);
 
-            
 
-         
+
+
 
             return "La sincronizacion ha terminado con exito";
 
